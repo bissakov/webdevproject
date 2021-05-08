8bf740bbcabc1869a4d754b20f9faf3beb8bd485
Alikhan Bissakov <bissakov@yandex.ru>
2021-05-08 12:17:53 +0600

    .

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,39 @@
 from django.contrib import admin
 from django.shortcuts import render, redirect
 from django.urls import path, include, re_path
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 def home(request):
     return render(request, 'store/index.html')
 
 def signin(request):
-    return render(request, 'store/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+
+    return render(request, 'store/login.html', {'form': form})
 
 def signup(request):
     if request.method == 'POST':
@@ -30,13 +51,19 @@ def signup(request):
     context = {'form' : form}
     return render(request, 'store/signup.html', context)
 
+def logout_view(request):
+    logout(request)
+    home()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('home', home),
     path('api/', include('store.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signin/', signin, name='login'),
     path('accounts/signup/', signup, name='signup'),
+    path('accounts/logout/', logout_view, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += [
